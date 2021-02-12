08634e0e04437e6b59d3879ad874ac846740810c
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-12 02:20:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function buttonOn() {
+  document.getElementByid("text").innerHTML =
+  "Done!"
+}
+
+function bridbuttOn() {
+  document.getElementById("bird-picture").src = "download.jpg"
+  document.getElementById("bird-picture").alt = "Blue Jay"
+  document.getElementById("background").style.backgroundColor = "Blue"
+}
