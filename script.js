3442d5ea8dd20beff4f1d589f61cdd1bcbd5d73d
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-12 04:36:50 +0000

    new errors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function buttonOn() {
-  document.getElementById("text").innerHTML = "Done!"
+function buttonOn () {
+  document.getElementById('text').innerHTML = 'Done!'
 }
 
-function dogbutton() {
-  document.getElementById("bird-picture").src = "download-1.jpg"
-  document.getElementById("bird-picture").alt = "dog picture"
-  document.getElementById("background").style.backgroundColor = "Blue"
+function dogbutton () {
+  document.getElementById('bird-picture').src = 'download-1.jpg'
+  document.getElementById('bird-picture').alt = 'dog picture'
+  document.getElementById('background').style.backgroundColor = 'Blue'
 }
