1aedb56631fa4418b1f0f0b2041df59bf2df3146
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-12 05:32:44 +0000

    Real Final Version!

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function dogbutton () {
   document.getElementById('bird-picture').alt = 'dog picture'
   document.getElementById('background').style.backgroundColor = 'Blue'
 }
+
+function disappearbutton () {
+  document.getElementById('disappear').style.display = 'none'
+}
+
+function myFunction() {
+  document.getElementById('paragraph').style.color = 'red'
+  document.getElementById('paragraph').style.fontSize = 'x-large'
+}
